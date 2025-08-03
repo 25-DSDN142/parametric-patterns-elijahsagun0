b6fadfde5097b2f393eb30d8911565ab28588a01
elijahsagun0 <elijahmarie.sagun@gmail.com>
2025-08-04 10:23:57 +1200

    wallpaper_background
    working on the dynamic of the background

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -13,49 +13,43 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
 }
+
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(239, 241, 243);
 }
 
-let rect_w = 30;
-let rect_h = 200;
-let rect_x = 0;
-let rect_y = 0;
-let gap = 50;
+let pos_x = 60;
+let pos_y = 100;
+petal_gap = 20;
+petal_x = 60;
+petal_y = 20;
 
 function my_symbol() { // do not rename
 //  this function. Treat this similarly to a Draw function
-  rect(rect_x, rect_y, rect_w, rect_h);
-  rect(rect_x+gap, rect_y, rect_w, rect_h);
-  rect(rect_x+(gap*2), rect_y, rect_w, rect_h);
-  rect(rect_x+(gap*3), rect_y, rect_w, rect_h);
-  rect(rect_x+(gap*4), rect_y, rect_w, rect_h);
-  drawBear(0, 0, 55); // calls the function to draw the bear
-  drawBear(90, 90, 60); 
+  noStroke();
+  fill(15, 113, 115); 
+  rect(0,0,100,100);
+  
+  fill(206, 229, 242);
+  rect(0, 100, 100, 100);
+  rect(100,0,100,100);
+  ellipse(50,pos_y, 100, 100);
+ 
+  fill(15, 113, 115);
+  rect(100, 100, 100, 100);
+
+  fill(206, 229, 242);
+  ellipse(150,pos_y, 100, 100);
+ 
+  //fill(15, 113, 115);
+  //drawFlower(pos_x, pos_y);
 }
 
-function drawBear(xPos, yPos, bear = 60, bearEarEye = bear/2, bearPupil= bear/3) {
-  translate(xPos, yPos);
-  
-  if (bear >= 60) {
-  stroke(41, 30, 24);
-  fill(168, 134, 113); 
-  } else {
-  fill(41, 30, 24); 
-  }
-
-  ellipse(60, 60, bear,bear);
-  ellipse(25, 30, bearEarEye, bearEarEye);
-  ellipse(95, 30, bearEarEye, bearEarEye);
-
-  fill (0);
-  ellipse(70, 60, bearEarEye, bearEarEye);
-  ellipse(35, 60, bearEarEye, bearEarEye);
-
-  fill (255);
-  ellipse(60, 60, bearPupil, bearPupil);
-  ellipse(25, 60, bearPupil, bearPupil);
+function drawFlower(x, y) {
+  fill(0, 128, 255); //light blue
+  ellipse(x, y, petal_x, petal_y);
+  ellipse(x+petal_gap+petal_x, y, petal_x, petal_y);
 }
