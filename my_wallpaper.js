3913d68e1edb1e88b0e895e35237a566864ac0a9
elijahsagun0 <elijahmarie.sagun@gmail.com>
2025-08-05 00:51:25 +1200

    wallpaper development

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -8,12 +8,14 @@ function setup_wallpaper(pWallpaper) {
   //pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
   pWallpaper.grid_settings.row_offset  = 0;
+  
+
 }
 
 
@@ -30,7 +32,7 @@ petal_y = 20;
 function my_symbol() { // do not rename
 //  this function. Treat this similarly to a Draw function
   noStroke();
-  fill(15, 113, 115); 
+  fill(25, 42, 81); 
   rect(0,0,100,100);
   
   fill(206, 229, 242);
@@ -38,12 +40,13 @@ function my_symbol() { // do not rename
   rect(100,0,100,100);
   ellipse(50,pos_y, 100, 100);
  
-  fill(15, 113, 115);
+  fill(25, 42, 81);
   rect(100, 100, 100, 100);
 
   fill(206, 229, 242);
   ellipse(150,pos_y, 100, 100);
  
+  drawFlower2(pos_x, pos_y);
   drawFlower(pos_x, pos_y);
 }
 
@@ -51,7 +54,18 @@ function drawFlower(x, y) {
   fill(0, 128, 255); //light blue
   ellipse(x, y, petal_x, petal_y);
   ellipse(x+petal_gap+petal_x, y, petal_x, petal_y);
-
   ellipse(100, y+petal_gap+petal_y, petal_y, petal_x);
   ellipse(100, 60, petal_y, petal_x);
 }
+
+function drawFlower2(x, y) {
+
+  noStroke();
+  fill(71, 160, 230); 
+  ellipse(100, 100, 15, 15);
+  stroke(0);
+  strokeWeight(0.5);
+  curve(100, 100, 100, 100, 150, 60, 200,200);
+  curve(100, 100, 100, 30, 60, 60, 200,200);
+}
+  
