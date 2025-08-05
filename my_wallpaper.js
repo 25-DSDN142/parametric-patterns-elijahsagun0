369a14b9c43d17a6886aff3220710404299f22d6
elijahsagun0 <elijahmarie.sagun@gmail.com>
2025-08-06 07:58:17 +1200

    Different parameters code

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,20 +1,28 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 30;
+let pos_x = 60;
+let pos_y = 100;
+let petal_gap = 20;
+let petal_x = 60;
+let petal_y = 20;
+let bgShapes = 100;
+let line_gap = 10;
+let line_height = 200;
+let line_width = 6;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
-  pWallpaper.resolution(FIT_TO_SCREEN);
-   //set this to false when you're ready to print
+  pWallpaper.resolution(A3);
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 0;
-  
+  pWallpaper.grid_settings.row_offset  = 50;
 
 }
 
@@ -23,39 +31,38 @@ function wallpaper_background() {
   background(239, 241, 243);
 }
 
-let pos_x = 60;
-let pos_y = 100;
-petal_gap = 20;
-petal_x = 60;
-petal_y = 20;
 
 function my_symbol() { // do not rename
 //  this function. Treat this similarly to a Draw function
   noStroke();
   fill(25, 42, 81); 
-  rect(0,0,100,100);
+  rect(0,0,bgShapes, bgShapes);
   
   lines(pos_x, pos_y); 
 
   fill(206, 229, 242);
-  rect(0, 100, 100, 100);
-  rect(100,0,100,100);
-  ellipse(50,pos_y, 100, 100);
+  rect(0, 100, bgShapes, bgShapes);
+  rect(100,0,bgShapes, bgShapes);
+  ellipse(50,pos_y, bgShapes, bgShapes);
  
   fill(25, 42, 81);
-  rect(100, 100, 100, 100);
+  rect(100, 100, bgShapes, bgShapes);
 
   lines(pos_x, pos_y);
 
   fill(206, 229, 242);
-  ellipse(150,pos_y, 100, 100);
+  ellipse(150,pos_y, bgShapes, bgShapes);
   
   drawFlower2(pos_x, pos_y);
   drawFlower(pos_x, pos_y);
 }
 
 function drawFlower(x, y) {
-  fill(0, 128, 255); //lightS blue
+  if (petal_x > 50) {
+    fill(0, 128, 255); //light blue
+  } else {
+    fill(25, 42, 81); //orange
+  }
   ellipse(x, y, petal_x, petal_y);
   ellipse(x+petal_gap+petal_x, y, petal_x, petal_y);
   ellipse(100, y+petal_gap+petal_y, petal_y, petal_x);
@@ -75,10 +82,14 @@ function drawFlower2(x, y) {
 }
 
 function lines(x,y) { 
-  fill(179, 221, 255);
-  rect(10, 0, 6, 200);
-  rect(20, 0, 6, 200);
-  rect(200-10, 0, 6, 200);
-  rect(200-20, 0, 6, 200);
+  if (line_gap > 5) {
+    fill(179, 221, 255);
+  } else {
+    fill(25, 42, 81);
+  }
+  rect(line_gap, 0, line_width, line_height);
+  rect(line_gap*2, 0, line_width, line_height);
+  rect(200-line_gap, 0, line_width, line_height);
+  rect(200-(line_gap*2), 0, line_width, line_height);
 }
   
